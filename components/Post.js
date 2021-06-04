0a258996e0db32f94d1d2c60d2e5c843fa8083f4
Carlos <carlos_gomes1809@hotmail.com>
2021-06-04 16:45:35 -0300

    animations added to posts

--- components/Post.js
+++ components/Post.js
@@ -1,60 +1,63 @@
 import { ChatAltIcon, ShareIcon, ThumbUpIcon } from "@heroicons/react/outline";
 import Image from "next/image";
+import { forwardRef } from "react";
 
-function Post({ name, message, email, postImage, image, timestamp }) {
-  return (
-    <div className="flex flex-col">
-      <div className="p-5 bg-white mt-5 rounded-t-2xl shadow-md">
-        <div className="flex items-center space-x-2">
-          <Image
-            className="rounded-full"
-            src={image}
-            width={40}
-            height={40}
-            alt=""
-          />
-          <div>
-            <p className="font-semibold">{name}</p>
-            <p className="text-xs text-gray-400">
-              {timestamp ? (
-                new Date(timestamp?.toDate()).toLocaleDateString()
-              ) : (
-                <p className="text-xs text-gray-400">Loading...</p>
-              )}
-            </p>
+const Post = forwardRef(
+  ({ name, message, email, postImage, image, timestamp }, ref) => {
+    return (
+      <div className="flex flex-col" ref={ref}>
+        <div className="p-5 bg-white mt-5 rounded-t-2xl shadow-md">
+          <div className="flex items-center space-x-2">
+            <Image
+              className="rounded-full"
+              src={image}
+              width={40}
+              height={40}
+              alt=""
+            />
+            <div>
+              <p className="font-semibold">{name}</p>
+              <p className="text-xs text-gray-400">
+                {timestamp ? (
+                  new Date(timestamp?.toDate()).toLocaleDateString()
+                ) : (
+                  <p className="text-xs text-gray-400">Loading...</p>
+                )}
+              </p>
+            </div>
           </div>
+          <p className="p-4">{message}</p>
         </div>
-        <p className="p-4">{message}</p>
-      </div>
-      {postImage && (
-        <div className="relative h-56 md:h-96 bg-white">
-          <Image src={postImage} objectFit="contain" layout="fill" />
-        </div>
-      )}
-      <div
-        className="flex 
+        {postImage && (
+          <div className="relative h-56 md:h-96 bg-white">
+            <Image src={postImage} objectFit="contain" layout="fill" />
+          </div>
+        )}
+        <div
+          className="flex 
         justify-between 
         items-center 
         rounded-b-2xl
          bg-white 
          shadow-md 
          text-gray-400 border-t"
-      >
-        <div className="inputIcon rounded-bl-2xl">
-          <ThumbUpIcon className="h-4" />
-          <p className="text-xs sm:text-base">Like</p>
-        </div>
-        <div className="inputIcon rounded-none">
-          <ChatAltIcon className="h-4" />
-          <p className="text-xs sm:text-base">Comment</p>
-        </div>
-        <div className="inputIcon rounded-br-2xl">
-          <ShareIcon className="h-4" />
-          <p className="text-xs sm:text-base">Share</p>
+        >
+          <div className="inputIcon rounded-bl-2xl">
+            <ThumbUpIcon className="h-4" />
+            <p className="text-xs sm:text-base">Like</p>
+          </div>
+          <div className="inputIcon rounded-none">
+            <ChatAltIcon className="h-4" />
+            <p className="text-xs sm:text-base">Comment</p>
+          </div>
+          <div className="inputIcon rounded-br-2xl">
+            <ShareIcon className="h-4" />
+            <p className="text-xs sm:text-base">Share</p>
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
+);
 
 export default Post;
--- components/Posts.js
+++ components/Posts.js
@@ -1,6 +1,7 @@
 import { useCollection } from "react-firebase-hooks/firestore";
 import { db } from "../firebase";
 import Post from "./Post";
+import FlipMove from "react-flip-move";
 
 function Posts({ posts }) {
   const [realtimePosts] = useCollection(
@@ -8,29 +9,31 @@ function Posts({ posts }) {
   );
   return (
     <div className="mb-20 mx-auto">
-      {realtimePosts
-        ? realtimePosts?.docs.map((post) => (
-            <Post
-              key={post.id}
-              name={post.data().name}
-              message={post.data().message}
-              email={post.data().email}
-              timestamp={post.data().timestamp}
-              image={post.data().image}
-              postImage={post.data().postImage}
-            />
-          ))
-        : posts.map((post) => (
-            <Post
-              key={post.id}
-              name={post.name}
-              message={post.message}
-              email={post.email}
-              timestamp={post.timestamp}
-              image={post.image}
-              postImage={post.postImage}
-            />
-          ))}
+      <FlipMove>
+        {realtimePosts
+          ? realtimePosts?.docs.map((post) => (
+              <Post
+                key={post.id}
+                name={post.data().name}
+                message={post.data().message}
+                email={post.data().email}
+                timestamp={post.data().timestamp}
+                image={post.data().image}
+                postImage={post.data().postImage}
+              />
+            ))
+          : posts.map((post) => (
+              <Post
+                key={post.id}
+                name={post.name}
+                message={post.message}
+                email={post.email}
+                timestamp={post.timestamp}
+                image={post.image}
+                postImage={post.postImage}
+              />
+            ))}
+      </FlipMove>
     </div>
   );
 }
